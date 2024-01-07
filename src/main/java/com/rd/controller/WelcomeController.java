777c0f7e48647cc564cf0e18b3282acbb315c7b7
RamdasDarwatkar <RamdasDarwatkar@IBM-B2ML3F3>
2024-01-07 13:19:23 +0530

    added dockerfile

---
+++ src/main/java/com/rd/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.rd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+	
+	@GetMapping("/docker")
+	public String welcomeToDocker() {
+		return "Welcome to world of containerized Applications...!";
+	}
+
+}
